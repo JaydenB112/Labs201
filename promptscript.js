f1569f3eb839317eb01b122e09803a22e1cc7786
Jayden Boyd <jaydenboydb1@gmail.com>
2023-02-20 15:32:46 -0600

    questions

--- promptscript.js
+++ promptscript.js
@@ -6,6 +6,8 @@ function Question () {
     alert("Yea okay we about to see.");
 
     let p3= prompt("What's your favorite team"+ p1 + "?");
+    alert("I wonder how confident you are.");
 
-    let p4=prompt("Do you see your team winning it all in 5 years?")
+    let p4=prompt("Do you see your team winning it all in 5 years?");
+    alert("Probably not because your team sucks, goodbye" + p1);  
 }
