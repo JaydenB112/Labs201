86ff64690b22658ec49c0cc6b00b9a16f8bd320b
Jayden Boyd <jaydenboydb1@gmail.com>
2023-02-20 15:14:00 -0600

    Made questions

---
+++ promptscript.js
@@ -0,0 +1,11 @@
+function Question () {
+    let p1= prompt("What is your name?");
+    alert("Hey "+ p1);
+
+    let p2= prompt("Do you watch the NFL?");
+    alert("Yea okay we about to see.");
+
+    let p3= prompt("What's your favorite team"+ p1 + "?");
+
+    let p4=prompt("Do you see your team winning it all in 5 years?")
+}
