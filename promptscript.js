670fd61c1f849640ef677cb0b1bf5dcdf9edd8d5
Jayden Boyd <jaydenboydb1@gmail.com>
2023-02-20 16:13:19 -0600

    tWEAKS

--- promptscript.js
+++ promptscript.js
@@ -1,13 +1,26 @@
-function Question () {
-    let p1= prompt("What is your name?");
-    alert("Hey "+ p1);
+let p1 = prompt("What is your name?");
+alert("Hey " + p1);
 
-    let p2= prompt("Do you watch the NFL?");
-    alert("Yea okay we about to see.");
+let p2 = prompt("Do you watch the NFL?");
+alert("Yea okay we about to see.");
 
-    let p3= prompt("What's your favorite team"+ p1 + "?");
-    alert("I wonder how confident you are.");
+let p3 = prompt("What's your favorite team " + p1 + "?");
+alert("I wonder how confident you are.");
 
-    let p4=prompt("Do you see your team winning it all in 5 years?");
-    alert("Probably not because your team sucks, goodbye" + p1);  
-}
+let p4 = prompt("Do you see your team winning it all in 5 years?");
+alert("Probably not because your team sucks, goodbye" + p1);  
+
+
+// function Question () {
+//     let p1= prompt("What is your name?");
+//     alert("Hey "+ p1);
+
+//     let p2= prompt("Do you watch the NFL?");
+//     alert("Yea okay we about to see.");
+
+//     let p3= prompt("What's your favorite team"+ p1 + "?");
+//     alert("I wonder how confident you are.");
+
+//     let p4=prompt("Do you see your team winning it all in 5 years?");
+//     alert("Probably not because your team sucks, goodbye" + p1);
+// }
