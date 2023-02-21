4b579fe65fef19ba2777ca6f06dc0d3de2891c81
Jayden Boyd <jaydenboydb1@gmail.com>
2023-02-20 19:38:15 -0600

    Switched Id and class names got css working

--- promptscript.js
+++ promptscript.js
@@ -1,26 +1,11 @@
-let p1 = prompt("What is your name?");
-alert("Hey " + p1);
+let p11 = prompt("What is your name?");
+alert("Hey " + userName);
 
-let p2 = prompt("Do you watch the NFL?");
+let p22 = prompt("Do you watch the NFL?");
 alert("Yea okay we about to see.");
 
-let p3 = prompt("What's your favorite team " + p1 + "?");
+let p33 = prompt("What's your favorite team " + userName + "?");
 alert("I wonder how confident you are.");
 
-let p4 = prompt("Do you see your team winning it all in 5 years?");
-alert("Probably not because your team sucks, goodbye" + p1);  
-
-
-// function Question () {
-//     let p1= prompt("What is your name?");
-//     alert("Hey "+ p1);
-
-//     let p2= prompt("Do you watch the NFL?");
-//     alert("Yea okay we about to see.");
-
-//     let p3= prompt("What's your favorite team"+ p1 + "?");
-//     alert("I wonder how confident you are.");
-
-//     let p4=prompt("Do you see your team winning it all in 5 years?");
-//     alert("Probably not because your team sucks, goodbye" + p1);
-// }
+let p44 = prompt("Do you see your team winning it all in 5 years?");
+alert("Probably not because your team sucks, goodbye " + userName);  
