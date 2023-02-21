7fa12af5c06c711de698eb38ced88354617d5ec5
Jayden Boyd <jaydenboydb1@gmail.com>
2023-02-20 19:51:29 -0600

    Changed name to match

--- promptscript.js
+++ promptscript.js
@@ -1,11 +1,11 @@
-let p11 = prompt("What is your name?");
+let userName = prompt("What is your name?");
 alert("Hey " + userName);
 
-let p22 = prompt("Do you watch the NFL?");
+let p2 = prompt("Do you watch the NFL?");
 alert("Yea okay we about to see.");
 
-let p33 = prompt("What's your favorite team " + userName + "?");
+let p3 = prompt("What's your favorite team " + userName + "?");
 alert("I wonder how confident you are.");
 
-let p44 = prompt("Do you see your team winning it all in 5 years?");
+let p4 = prompt("Do you see your team winning it all in 5 years?");
 alert("Probably not because your team sucks, goodbye " + userName);  
